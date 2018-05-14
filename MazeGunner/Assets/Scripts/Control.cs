964e0a8810d5cbf37ffb93843eaf071de2f4a2bd
DESKTOP-5QSEO5K\FliQside <raymond.a.lemay@gmail.com>
2018-05-13 17:15:30 -0700

    Added loadscene

---
+++ MazeGunner/Assets/Scripts/Control.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Control : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+    // Change scene to start screen
+    // Pre: Cannot already be on start screen
+    // Post: Displays Start Screen
+    public void ToStartScreen()
+    {
+        SceneManager.LoadScene("StartScreen");
+    }
+
+
+
+}
